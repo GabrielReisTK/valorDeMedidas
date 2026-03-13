356dea89eb933d541ba2c10033e3f00d318cb0f4
Gabriel Reis <contato.gabrielreis2006@gmail.com>
2026-03-13 14:41:16 -0300

    add

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,33 @@
-public class main {
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        try (Scanner sc = new java.util.Scanner(System.in)) {
+            System.out.println("\n--- Unidade de medidas ---");
+            System.out.println("1- converter em Km/h");
+            System.out.println("2- converter em cm");
+
+            String opcao = sc.next();
+            double valores;
+            double resultado;
+
+            switch (opcao){
+                case "1":
+                    System.out.println("Digite o valor que quer converter de metros para km/h");
+                    valores = sc.nextDouble();
+                    resultado = valores / 1000;
+                    System.out.println("O resultado foi " + resultado + "km/h");
+                    break;
+                case "2":
+                    System.out.println("Digite o valor que quer converter de metros para cm");
+                    valores = sc.nextDouble();
+                    resultado = valores * 100;
+                    System.out.println("O resultado foi " + resultado + "cm");
+                    break;
+                default:
+                    System.out.println("Valores inválidos, tente novamente");
+            }
+
+        }
+    }
 }
